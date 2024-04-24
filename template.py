c510de688f1ac4e5e051e4e112b74212f2e2f60e
Darshan0312 <darshanbr081@gmail.com>
2024-04-24 13:00:46 +0530

    notebook updated

--- template.py
+++ template.py
@@ -1,33 +1,43 @@
 import os
 from pathlib import Path
 
-package_name = "mongodb_connect"
 
-list_of_files = [
-   ".github/workflows/ci.yaml",
-   "src/__init__.py",
-   f"src/{package_name}/__init__.py", 
-   f"src/{package_name}/mongo_crud.py", 
-   "tests/__init__.py",
-   "tests/unit/__init__.py",
-   "tests/integration/__init__.py",
-   "init_setup.sh",
-   "requirements.txt", 
-   "setup.py",
-   "setup.cfg",
-   "pyproject.toml",
-   "tox.ini",
-   "experiments/experiments.ipynb",
+list_of_files =[
+
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "__init__setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.in",
+    "experiment"
+
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        
+
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
-            pass # create an empty file
-
-#its updated
+        with open(filepath,"w") as f:
+            pass #create a empty file
